c8b174989c3fd36d078c662d83ed67684ab97215
satish sharma <satish.sharma>
2025-07-26 21:41:55 +0530

    AddedVersionControledFile

---
+++ version.c
@@ -0,0 +1,11 @@
+/***********************************************************
+ * @brief version.c
+ **********************************************************/
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define MAJOR_VERSION "00"
+#define MINOR_VERSION "01"
+
+#endif
